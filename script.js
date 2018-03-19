1c8eed49f1aac31b46f3dfd7188285305eb0bcc7
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-19 14:42:47 -0400

    Select random square and present its color value in span. Add event listeners to all squares and check clicked square color against selected color

--- script.js
+++ script.js
@@ -11,6 +11,30 @@ function randomColor(){
 let squares = document.getElementsByClassName('squares')
 
 for (var i = 0; i < squares.length; i++) {
+  //assign random color
   squares[i].style.backgroundColor = randomColor();
+
+  //add event listener on each square
+  //check to see if color is the selected color
+  squares[i].addEventListener("click", function(){
+    if (this.style.backgroundColor === presentColor.innerText) {
+      alert("Correct!")
+    } else {
+      alert("Nope!")
+    }
+  })
 }
 
+let selectColor = squares[Math.floor(Math.random() * squares.length)].style.backgroundColor
+
+let presentColor = document.getElementById("rgb")
+
+presentColor.innerText = selectColor
+
+// function shuffleArray(array) {
+//   for (let i = array.length - 1; i > 0; i--) {
+//       let j = Math.floor(Math.random() * (i + 1));
+//       [array[i], array[j]] = [array[j], array[i]];
+//   }
+
+// }
